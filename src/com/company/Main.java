c91834c939c857787f4ece4292640e2b5e0e5c75
jarzombeg@gmail.com <XJZZK3C6kpC4Wc4>
2021-02-06 19:44:41 +0000

    First

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiusToFahrenheit(41));
+    }
+
+    public static double convertCelsiusToFahrenheit(int celsius) {
+        //write your code here
+        double one = 9;
+        int two = 5;
+        double bracket = one / two;
+        return bracket * celsius + 32;
+    }
+}
+
+
