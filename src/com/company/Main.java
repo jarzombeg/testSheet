277c9c49a8825cdda4c57a0bbd4b6613c6454e3c
jarzombeg@gmail.com <XJZZK3C6kpC4Wc4>
2021-02-07 19:50:37 +0000

    First

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,65 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(convertCelsiusToFahrenheit(41));
+/*    public static void main(String[] args) {
+        Student jen = new Student();
+        jen.name = "Jen";
+        jen.age = 21;
+
+        Student beth = new Student();
+        beth.name = "Beth";
+        beth.age = 15;
+
+        System.out.println("Jen is " + jen.age);
+        System.out.println("Beth is " + beth.age);
+
+        ageSwap(jen, beth);
+
+        System.out.println("Jen is " + jen.age);
+        System.out.println("Beth is " + beth.age);
     }
 
-    public static double convertCelsiusToFahrenheit(int celsius) {
-        //write your code here
-        double one = 9;
-        int two = 5;
-        double bracket = one / two;
-        return bracket * celsius + 32;
+    private static void ageSwap(Student a,
+                                Student b)
+    {
+        int c = a.age;
+        a.age = b.age;
+        b.age = c;
+    }
+
+    static class Student
+    {
+        String name;
+        int age;
+    }
+}*/
+
+    public static void main(String[] args)
+    {
+        Student jen = new Student();
+        jen.name = "Jen";
+        jen.age = 21;
+
+        Student beth = new Student();
+        beth.name = "Beth";
+        beth.age = 15;
+
+        System.out.println("Jen is " + jen.age);
+        System.out.println("Beth is " + beth.age);
+
+        Student a = jen, b = beth;
+
+        int c = a.age;
+        a.age = b.age;
+        b.age = c;
+
+        System.out.println("Jen is " + jen.age);
+        System.out.println("Beth is " + beth.age);
+    }
+
+    static class Student
+    {
+        String name;
+        int age;
     }
 }
-
-
