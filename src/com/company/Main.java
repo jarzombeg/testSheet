2582b45dcd7c193da9f43c7762bdccab0f04e3e3
jarzombeg <jarzombeg@gmail.com>
2021-02-09 18:17:30 +0000

    First

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,43 @@ package com.company;
 
 
 public class Main {
-     public static int min(int a, int b, int c) {
+    public static int min(int a, int b, int c, int d) {
+        //write your code here
+        int m;
+        if (a <= b && a < c && a <= d)
+            m = a;
+        else if (b <= a && b <= c && b <= d)
+            m = b;
+        else if (c <= a && c <= b && c <= d)
+            m = c;
+        else m = d;
+        return m;
+
+
+    }
+
+    public static int min(int a, int b) {
+        //write your code here
+        int n;
+        if (a <= b)
+            n = a;
+        else n = b;
+        return n;
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-20, -10, -30, -40));
+        System.out.println(min(-20, -10, -30, 40));
+        System.out.println(min(-40, -10, -30, 40));
+
+    }
+}
+
+
+    /*public static int min(int a, int b, int c) {
         //write your code here
         int m;
         if (a <= b && a <= c )
@@ -20,7 +56,7 @@ public class Main {
         System.out.println(min(5, 5, 10));
     }
 
-}
+}*/
 
 
 
