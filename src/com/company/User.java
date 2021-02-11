8ccef21897f8cb71ca7c6dbe7b028c05d04a6c6c
jarzombeg <jarzombeg@gmail.com>
2021-02-11 12:11:11 +0000

    First

---
+++ src/com/company/User.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class User {
+
+    String name;
+    short age;
+    int height;
+
+
+// Write your code here
+
+
+    public static void main(String[] args) {
+
+    }
+}
