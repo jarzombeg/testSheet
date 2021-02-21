2715816750c5a500f616c1e67e7ff8fdcbd6fb70
jarzombeg <jarzombeg@gmail.com>
2021-02-21 17:53:48 +0000

    First

---
+++ src/com/company/Solution.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code
+        int[] horizontalNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineOne = horizontalNumbers[i] + " ";
+            System.out.print(lineOne);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineTwo = (horizontalNumbers[i] * horizontalNumbers[1]) + " ";
+            System.out.print(lineTwo);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineThree = (horizontalNumbers[i] * horizontalNumbers[2]) + " ";
+            System.out.print(lineThree);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineFour = (horizontalNumbers[i] * horizontalNumbers[3]) + " ";
+            System.out.print(lineFour);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineFive = (horizontalNumbers[i] * horizontalNumbers[4]) + " ";
+            System.out.print(lineFive);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineSix = (horizontalNumbers[i] * horizontalNumbers[5]) + " ";
+            System.out.print(lineSix);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineSeven = (horizontalNumbers[i] * horizontalNumbers[6]) + " ";
+            System.out.print(lineSeven);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineEight = (horizontalNumbers[i] * horizontalNumbers[7]) + " ";
+            System.out.print(lineEight);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineNine = (horizontalNumbers[i] * horizontalNumbers[8]) + " ";
+            System.out.print(lineNine);
+        }
+        System.out.println();
+        for (int i = 0; i < horizontalNumbers.length; i++) {
+            String lineTen = (horizontalNumbers[i] * horizontalNumbers[9]) + " ";
+            System.out.print(lineTen);
+        }
+        }
+    }
