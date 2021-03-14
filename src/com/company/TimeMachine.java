78a616fc30c1f94e8a14a9b0a190ca55f56a1ae8
jarzombeg <jarzombeg@gmail.com>
2021-03-14 16:50:08 +0000

    First

--- src/com/company/TimeMachine.java
+++ src/com/company/TimeMachine.java
@@ -1,32 +1,15 @@
 package com.company;
+import java.io.*;
 
 public class TimeMachine {
 
-        public void goToFuture (Cat cat){
-            //cat = new Cat(cat.age);    //uncomment to break it
-            cat.age += 10;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s = input.readLine();
+            if (s.equals("exit"))
+                break;
         }
-
-        public void goToPast (Cat cat){
-            //cat = new Cat(cat.age);    //uncomment to break it
-            cat.age -= 10;
-        }
-
-    public static void main(String[] args) {
-
-        TimeMachine timeMachine = new TimeMachine();
-
-        Cat smudge = new Cat(5);
-
-        System.out.println("How old was Smudge when the program started?");
-        System.out.println(smudge.age);
-
-        timeMachine.goToFuture(smudge);
-        System.out.println ("Smudge went to the future! Has his age changed?");
-        System.out.println(smudge.age);
-
-        System.out.println ("And if you try going back?");
-        timeMachine.goToPast(smudge);
-        System.out.println(smudge.age);
     }
 }
